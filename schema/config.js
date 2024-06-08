fb47e0d765bc7af8a2b50dc1ba064532f839abc5
KiZakiXD <88037146+KiZakiXD@users.noreply.github.com>
2024-06-09 02:37:56 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,15 +1,15 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "ZeyAPIs",
+  developer: "Follow Instagram : @iamkizakixd",
   port: 3000,
-  webName: "HitamAPIs Playground",
-  description: "Simple Base Rest API",
+  webName: "ZeyAPIs Playground",
+  description: "Simple Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://api-flax-chi-68.vercel.app", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://base-api-plum.vercel.app/", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
